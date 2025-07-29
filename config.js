23c7557a040592051c8d1d0708d970058cf13bc0
Alfonso Graterol <alfonsograterolpow@gmail.com>
2025-07-29 18:05:12 -0500

    whatsapp plus

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
-  ownerNumber: 'NUMERO_DEL_PROPIETARIO',
-  botName: 'NOMBRE_DEL_BOT',
-  ownerName: 'NOMBRE_DEL_PROPIETARIO',
+  ownerNumber: '573146171942',
+  botName: 'whatsapp plus',
+  ownerName: 'alfo',
   prefixes: ['/', '.', '#', '>'],
   logChannel: 'ID_DEL_CANAL@newsletter', // Reemplaza esto con el ID de tu canal de logs
   defaultTime: 10 // minutos por defecto para el tiempo de usuarios
